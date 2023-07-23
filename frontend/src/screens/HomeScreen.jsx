17c66159de4e5151b23a5ea42397d963c2e7ea44
Revant <revant202@gmail.com>
2023-07-23 23:07:05 +0530

    updated frontend and backend

--- frontend/src/screens/HomeScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Profile } from '../components/Profile';
-import { Navbar } from '../components/Navbar';
-export default function Home() {
-    return <>
-    <Navbar></Navbar>
-    <Profile></Profile>
-    </>
-}
